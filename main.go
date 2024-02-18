505cb6b7e49f04f1601e78e34689075181aed221
Nik Swap <nikswap@local.host>
2024-02-18 08:37:50 +0100

    Added code

---
+++ main.go
@@ -0,0 +1,46 @@
+//Decode output from the server on *nix:
+//for i in $(seq 0 15); do curl -s -w "%{time_total}" -o /dev/null 'http://localhost:3333?bitNumber='"$i" | awk -F '.' '{print $1}'; done | tr '31' '01' | tr -d '\n'
+
+/*
+TODO:
+   * Implement checksum
+   * Implement more time delay for sending different letters
+*/
+
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+	"strconv"
+	"fmt"
+)
+
+
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func testSendMessage(w http.ResponseWriter, r *http.Request) {
+	bitVal := r.URL.Query().Get("bitNumber")
+	message := "0100100001001001"
+	bitCount,err := strconv.Atoi(bitVal)
+	CheckError(err)
+	timeToWait := 3
+	if message[bitCount] == 49 {
+		timeToWait = 1
+	}
+	fmt.Printf("Sending %d\n", message[bitCount])
+	time.Sleep(time.Duration(timeToWait) * time.Second)
+	io.WriteString(w, "OK")
+}
+
+func main() {
+	http.HandleFunc("/", testSendMessage)
+	err := http.ListenAndServe(":3333", nil)
+	CheckError(err)
+}
